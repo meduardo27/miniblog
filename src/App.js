66b41cc59ca94640a4eed13b84c2e2984deb490e
Mateus Eduardo <medu2710@gmail.com>
2023-03-25 14:55:43 -0300

    feat: manter post

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import { onAuthStateChanged } from "firebase/auth";
-
 import { useState, useEffect } from "react";
 import { useAuthentication } from "./hooks/useAuthentication";
-
 import { AuthProvider } from "./context/AuthContext";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -45,6 +44,7 @@ function App() {
               <Route path="/" element={<Home />}></Route>
               <Route path="/about" element={<About />}></Route>
               <Route path="/search" element={<Search />}></Route>
+              <Route path="/posts/:id" element={<Post />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
